ee37594b77a6d276e9f80983932cd2c721ba88da
Артем Павлов <artem.pavlov.2006@mail.ru>
2024-08-01 18:11:05 +0300

    lessons

---
+++ lessons/l1.js
@@ -0,0 +1,38 @@
+'use strict';
+//let a="";
+//a=prompt('Введите ваше имя');
+//alert('Привет,'+a+'!')
+
+const a =prompt('Введите ваше имя');
+alert('Привет,'+a+'!');
+
+const
+    bird=prompt('Введте год рождения',2000),
+    mes=prompt('Введте месяц рождения'),
+    day=prompt('Введте день рождения'),
+    year=2024,
+    age=year-bird;
+
+alert('Ваш возраст'+age);
+//let f=true;
+//while(f)
+//{
+//    if(mes>36)
+//    {
+//        mes-=36;
+//        bird=bird+1;
+//    }
+//    else
+///    {
+ //       if(day>30)
+ //       {
+ //           day=day-30;
+ //           mes=mes+1;
+ //       }
+ //       else
+//        {
+//          f=false;
+//        }
+//    }
+//}
+//const age=year-bird;
